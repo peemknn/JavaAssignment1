f12da99b2f50f476ac93897b1be037df84b7afb6
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-05 23:08:44 +0700

    Update Diamond.java

--- src/logic/Diamond.java
+++ src/logic/Diamond.java
@@ -2,35 +2,17 @@ package logic;
 
 public class Diamond {
 	public void diamondShape(int n) {
-		int spaces = n / 2;
-        int stars = 1;
-        int gap = n-1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < spaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < gap; k++) {
-            	System.out.print(" ");
-            }
-
-            for (int l = 0; l < stars; l++) {
-                System.out.print("+ ");
-            }
-
-            System.out.println();
-
-            if (i < n / 2) {
-                spaces--;
-                gap--;
-                stars += 2;
-            } else {
-                spaces++;
-                gap++;
-                stars -= 2;
-            }
-        
-            
-        } 
+		System.out.println("Display Diamond: "+ n);
+		final String star = "+ ";
+		final String space = "  ";
+		StringEdit edit = new StringEdit();
+		
+		for(int i = n/2; i >= 1;i--) {
+		System.out.println(edit.repeat(space, i) + edit.repeat(star, n - 2*i) + edit.repeat(space, i));
+		}
+		System.out.println(edit.repeat(star, n));
+		for(int i = 1; i<(n/2)+1;i++) {
+			System.out.println(edit.repeat(space, i) + edit.repeat(star, n - 2*i) + edit.repeat(space, i));
+		}
 	}
 }
