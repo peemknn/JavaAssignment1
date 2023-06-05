3fbe1d220f15fc8ec7c7b964b29826ca378483d8
Peemknn <kunanontpeem333@gmail.com>
2023-06-05 15:52:24 +0700

    Implement logic

--- src/logic/Diamond.java
+++ src/logic/Diamond.java
@@ -1,37 +1,36 @@
 package logic;
 
 public class Diamond {
-	String result = "";
 	public void diamondShape(int n) {
 		int spaces = n / 2;
         int stars = 1;
+        int gap = n-1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < spaces; j++) {
                 System.out.print(" ");
             }
+            for (int k = 0; k < gap; k++) {
+            	System.out.print(" ");
+            }
 
-            for (int k = 0; k < stars; k++) {
-                System.out.print("+");
+            for (int l = 0; l < stars; l++) {
+                System.out.print("+ ");
             }
 
             System.out.println();
 
             if (i < n / 2) {
                 spaces--;
+                gap--;
                 stars += 2;
             } else {
                 spaces++;
+                gap++;
                 stars -= 2;
             }
-        }
-
-        System.out.println("n"); 
-		
-		
+        
+            
+        } 
 	}
-	public void printDiamond() {
-		System.out.println(result);
-	}
-
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,49 +1,47 @@
 package main;
 
-import java.util.*;
+import java.util.Scanner;
 
 import logic.Diamond;
+import logic.Butterfly;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		while(true) {
+		while (true) {
 			try {
 				Scanner scanner = new Scanner(System.in);
 				System.out.println("Enter a type of shape and n:");
 				String[] input = scanner.nextLine().split(" ");
 				String shape = input[0];
 				int n = Integer.parseInt(input[1]);
-				
-				if(shape.equals("Diamond")){
-					if(n%2 == 0 ) {
+
+				if (shape.equals("Diamond")) {
+					if (n % 2 == 0) {
 						throw new Exception();
-					}else if(n <=0 || n > 50){
+					} else if (n <= 0 || n > 50) {
 						throw new Exception();
-					}else {
-						System.out.println("Pass");
+					} else {
 						Diamond diamond = new Diamond();
 						diamond.diamondShape(n);
-						diamond.printDiamond();
-						
 						break;
 					}
-				}else if(shape.equals("Butterfly")){
-					if(n%2 != 0 ) {
+				} else if (shape.equals("Butterfly")) {
+					if (n % 2 != 0) {
 						throw new Exception();
-					}else if(n <=0 || n > 50){
+					} else if (n <= 0 || n > 50) {
 						throw new Exception();
-					}else {
-						System.out.println("Pass");
+					} else {
+						Butterfly butterfly = new Butterfly();
+						butterfly.butterflyShape(n);
 						break;
 					}
-				}else {
+				} else {
 					throw new Exception();
-			}
-				
-				
-			}catch(Exception e) {
+				}
+
+			} catch (Exception e) {
 				System.out.println("Invalid input, Please try again.");
 			}
 		}
