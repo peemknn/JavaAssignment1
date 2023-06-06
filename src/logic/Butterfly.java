caa251f5a8231aa4c819efef52988807572be9de
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-06 11:25:12 +0700

    Update butterfly display

--- src/logic/Butterfly.java
+++ src/logic/Butterfly.java
@@ -10,15 +10,15 @@ public class Butterfly {
 		
 		// top
 		for (int i = 1; i < n; i++) {
-			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i));
+			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i) + "\n");
 		}
 
 		// middle
-		System.out.println(str.repeat(star, n) + "- " + str.repeat(star, n));
+		System.out.println(str.repeat(star, n) + "- " + str.repeat(star, n) + "\n");
 
 		// bottom
 		for (int i = n - 1; i >= 1; i--) {
-			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i));
+			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i) + "\n");
 		}
 		System.out.println();
 		System.out.println("This is butterfly: " + n + "\n");
