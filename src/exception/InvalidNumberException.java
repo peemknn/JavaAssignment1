780ffb465f42cc5c92ebdb2ba2e81ecba4bfca37
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-06 14:19:57 +0700

    Update InvalidNumberException.java

--- src/exception/InvalidNumberException.java
+++ src/exception/InvalidNumberException.java
@@ -1,26 +1,11 @@
-package logic;
+package exception;
 
-public class Diamond {
-	private final String star = "+ ";
-	private final String space = "  ";
+public class InvalidNumberException extends Exception{
 
-	public void diamondShape(int n) {
-
-		StringEdit str = new StringEdit();
-		System.out.println();
-
-		// top
-		for (int i = n / 2; i >= 1; i--) {
-			System.out.println(str.repeat(space, i) + str.repeat(star, n - 2 * i) + str.repeat(space, i) + "\n");
-		}
-		// middle
-		System.out.println(str.repeat(star, n) + "\n");
-
-		// bottom
-		for (int i = 1; i < (n / 2) + 1; i++) {
-			System.out.println(str.repeat(space, i) + str.repeat(star, n - 2 * i) + str.repeat(space, i) + "\n");
-		}
-		System.out.println();
-		System.out.println("This is diamond: " + n + "\n");
+	public InvalidNumberException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
 	}
+
 }
+
