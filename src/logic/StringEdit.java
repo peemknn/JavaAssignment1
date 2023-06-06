e24407c789b27819a0d8a2171fadcf6c3d1b566d
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-06 09:03:06 +0700

    Update StringEdit.java

--- src/logic/StringEdit.java
+++ src/logic/StringEdit.java
@@ -1,15 +1,20 @@
 package logic;
 
 public class StringEdit {
+	private String res = "";
+	
 	//function for repeat a string
-	//ex. repeat("abc",3) will be equal to "abcabcabc"
+	//example : repeat("aa",3) will be equal to "aaaaaa"
 	public String repeat(String input, int n) {
-		String res = "";
+		setRes("");
 		for (int i = 0; i < n; i++) {
 			res += input;
 		}
 		return res;
 
 	}
-
+	public void setRes(String res) {
+		this.res = res;
+	}
 }
+
