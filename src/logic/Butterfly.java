c0a1cbd4ef34c69e58efd03b4eba2be87f7a6079
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-06 10:17:51 +0700

    Update Butterfly.java

--- src/logic/Butterfly.java
+++ src/logic/Butterfly.java
@@ -1,24 +1,27 @@
 package logic;
 
 public class Butterfly {
+	private final String star = "+ ";
+	private final String space = "  ";
 	public void butterflyShape(int n) {
-		System.out.println();
-		final String star = "+ ";
-		final String space = "  ";
-		StringEdit str = new StringEdit();
 		
-		//top
+		StringEdit str = new StringEdit();
+		System.out.println();
+		
+		// top
 		for (int i = 1; i < n; i++) {
 			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i));
 		}
-		//middle
+
+		// middle
 		System.out.println(str.repeat(star, n) + "- " + str.repeat(star, n));
-		
-		//bottom
+
+		// bottom
 		for (int i = n - 1; i >= 1; i--) {
 			System.out.println(str.repeat(star, i) + str.repeat(space, 2 * (n - i) + 1) + str.repeat(star, i));
 		}
 		System.out.println();
 		System.out.println("This is butterfly: " + n + "\n");
-	}	
+	}
+
 }
