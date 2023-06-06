9dfb6f68ae3f5eaf631e16cb5d8bbf3332ece3f4
peemknn <60140824+peemknn@users.noreply.github.com>
2023-06-06 09:11:53 +0700

    Final commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
 import java.util.Scanner;
-
+import exception.InvalidNumberException;
 import logic.Diamond;
 import logic.Butterfly;
 
@@ -9,50 +9,56 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
 		while (true) {
 			try {
 				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter a type of shape and n:");
-				//input shape and n and them split into 2 variables
+				System.out.println("Please enter a type of shape and n:");
+				
+				// split into shape type and int n
 				String[] input = scanner.nextLine().split(" ");
+				if(input[0].equals("close")) {
+					System.out.println("Bye...");
+					break;
+				}
 				String shape = input[0];
 				int n = Integer.parseInt(input[1]);
 				
-				//input shape is diamond
+
+				//if input shape is diamond
 				if (shape.equals("Diamond")) {
-					//if n is even
-					if (n % 2 == 0) {
-						throw new Exception();
-					//if n is not between 1 and 50
-					} else if (n <= 0 || n > 50) {
-						throw new Exception();
+					// n should be between 1 and 50
+					if (n <= 0 || n > 50) {
+						throw new InvalidNumberException("n should be between 1 and 50.");
+					// n should be odd with diamond
+					} else if (n % 2 == 0) {
+						throw new InvalidNumberException("With diamond shape, n should be odd.");
+					// all input are valid , Print shape
 					} else {
 						Diamond diamond = new Diamond();
 						diamond.diamondShape(n);
-						break;
 					}
-				//input shape is butterfly
+				// if input shape is butterfly
 				} else if (shape.equals("Butterfly")) {
-					//if n is even
-					if (n % 2 != 0) {
-						throw new Exception();
-					//if n is not between 1 and 50
-					} else if (n <= 0 || n > 50) {
-						throw new Exception();
-					
+					// n should be between 1 and 50
+					if (n <= 0 || n > 50) {
+						throw new InvalidNumberException("n should be between 1 and 50.");
+					// n should be even with butterfly shape
+					} else if (n % 2 != 0) {
+						throw new InvalidNumberException("With butterfly shape, n should be even.");
+					// all input are valid , Print shape
 					} else {
 						Butterfly butterfly = new Butterfly();
 						butterfly.butterflyShape(n);
-						break;
 					}
+				
 				} else {
-					//input shape is invalid
 					throw new Exception();
 				}
-
-			} catch (Exception e) {
-				//error message
-				System.out.println("Invalid input, Please try again.");
+			}catch (InvalidNumberException e) {
+				System.out.println("Error: " + e.getMessage());
+			}catch (Exception e) {
+				System.out.println("Error : Invalid input");
 			}
 		}
 
